b9f50de805b322fd374c3a63e30eee6e3357286f
theshivammittal <theshivammittal@gmail.com>
2025-07-03 22:40:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Grab elements from the page
-const button = document.getElementById("toggleBtn");
-const text = document.getElementById("toolText");
-
-// Add click event listener
-button.addEventListener("click", function () {
-  // Toggle visibility
-  if (text.classList.contains("hidden")) {
-    text.classList.remove("hidden");
-    button.textContent = "Hide Tool";
-  } else {
-    text.classList.add("hidden");
-    button.textContent = "Show Tool";
-  }
-});
-
-$(document).ready(function () {
-  $("#toggleSkills").click(function () {
-    $("#skillsList").slideToggle(); // Toggle the skills list
-  });
+document.getElementById("hireBtn").addEventListener("click", function () {
+  document.getElementById("thankMsg").classList.remove("hidden");
 });
